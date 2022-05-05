fafba4c10fd019af80ecf9bc654da56f90aa2687
DaisukeMatsuura <mail@brightful.jp>
2022-05-05 21:57:26 +0900

    add: Error component

---
+++ components/page/Login/Errors.tsx
@@ -0,0 +1,16 @@
+export const Errors = ({ errors = [], ...props }) => {
+  return (
+    <>
+      {errors.length > 0 && (
+        <div {...props}>
+          <div className="text-red-700 font-bold">Error</div>
+          <ul className="mt-2 text-red-700 text-sm">
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
